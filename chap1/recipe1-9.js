864df341ceb83d641ff7d429e660c043690e8569
cjcj5438 <1002931123@qq.com>
2017-10-18 23:39:34 +0800

    人

--- chap1/recipe1-9.js
+++ chap1/recipe1-9.js
@@ -1,10 +1,12 @@
 var dtstr= "2014-3-04T19:35:32Z";
 
-dtstr = dtstr.replace(/\D/g," ");
+dtstr = dtstr.replace(/\D/g," "); //TODO:\D匹配非数字的字符 同时替换成空字符串
 var dtcomps = dtstr.split(" ");
 
 // modify month between 1 based ISO 8601 and zero based Date
-dtcomps[1]--;
+// debugger;
+debug
+dtcomps[1]--;//月份减一
 
 var convdt = new Date(Date.UTC.apply(null,dtcomps));
 
